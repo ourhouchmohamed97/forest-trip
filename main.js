3508ef78fa37cfa6c8018d9ee2a611e6417c3742
Mohamed-Ourhouch <ourhouchmohamed97@gmail.com>
2023-11-08 20:40:47 +0100

    trip commit

---
+++ main.js
@@ -0,0 +1,17 @@
+//swiper code
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.next-btn',
+        prevEl: '.prev-btn',
+    },
+});
+
+
+
